5e615d93d5d9814940a0e70dc93a20a5cc907493
Sam Joseph <tansaku@gmail.com>
2014-06-18 17:54:59 +0100

    some more cucumber

--- features/bookmark_list.feature
+++ features/bookmark_list.feature
@@ -5,5 +5,12 @@ Feature: Bookmark List
 
 	Scenario: When there are no bookmarks
 		Given there are no bookmarks
-		When I am on the homepage
+		When I visit the homepage
 		Then I should see "There are currently no bookmarks"
+
+  Scenario: Adding a bookmark
+    Given I want to store "www.makersacademy.com"
+    And I visit the homepage
+    When I add the bookmark
+    Then I should see see "Makers Academy"
+
--- features/step_definitions/bookmark_steps.rb
+++ features/step_definitions/bookmark_steps.rb
@@ -1,10 +1,25 @@
+
 Given(/^there are no bookmarks$/) do
+    # Link.destroy if Link
 end
 
-When(/^I am on the homepage$/) do
+When(/^I visit the homepage$/) do
   visit '/'
 end
 
 Then(/^I should see "(.*?)"$/) do |text|
   expect(page).to have_content text
 end
+
+Given(/^I want to store "(.*?)"$/) do |url|
+  
+end
+
+When(/^I add the bookmark$/) do
+  click_button 'Submit'
+end
+
+Then(/^I should see see "(.*?)"$/) do |arg1|
+  expect(page).to have_content arg1
+end
+
